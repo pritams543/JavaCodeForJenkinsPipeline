d82ebd20b4968b8ca323ec488d5c2d9a00bda86a
pritams543 <pritam.sapkal@zensar.com>
2025-07-16 15:47:35 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ import java.util.*;
 class Test{
   public static void main(String[] args){
     System.out.println("Hello from Jenkins Pipeline");
-    System.out.println("Today is : "+LocalDataTime.now());
+    System.out.println("Today is : "+LocalDateTime.now());
   }
 }
