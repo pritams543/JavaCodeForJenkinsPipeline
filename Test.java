032d3be34f18acb741cfc1de353a855817ffd03d
pritams543 <pritam.sapkal@zensar.com>
2025-11-17 12:01:40 +0530

    Update date output to use LocalDate instead of LocalDateTime

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ import java.time.*;
 class Test{
   public static void main(String[] args){
     System.out.println("Hello from Jenkins Pipeline");
-    System.out.println("Today is : "+LocalDateTime.now());
+    System.out.println("Today is : "+LocalDate.now());
+    System.out.println("Thank you for using Jenkins...");
   }
 }
